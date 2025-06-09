4c35b9829f1b523d5b21975b3289e140a97de183
22120098 <phananhhao22012004@gmail.com>
2025-06-10 02:05:31 +0700

    update: CORS middleware to allow all origins

--- app/backend/main.py
+++ app/backend/main.py
@@ -2,21 +2,13 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict
 from get_jobs import get_jobs_endpoint
-import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 app = FastAPI()
 
-# Get allowed origins from .env
-allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
-
-# Add CORS middleware
+# Add CORS middleware to allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,  # Allow origins from .env
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
