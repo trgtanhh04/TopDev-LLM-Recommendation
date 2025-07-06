b6d0db127bb994756b5f5d14de1e11bc1b0b3024
22120098 <phananhhao22012004@gmail.com>
2025-07-06 13:55:44 +0700

    update

--- app/frontend/src/router.js
+++ app/frontend/src/router.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 import JobList from './components/JobList.vue';
 import JobDetail from './components/JobDetail.vue';
 import ProcessCV from './components/FindJobCV.vue';
@@ -7,11 +6,6 @@ import CompanyDetail from './components/CompanyDetail.vue';
 import HomePage from './components/HomePage.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HelloWorld,
-  },
   {
     path: '/viec-lam-it',
     name: 'Jobs',
